718b76340482fb8cc7600479e2e2ef3fd18a01d1
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-05-02 23:19:32 -0400

    added map

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-
 import Loader from './components/Loader';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className=" bg-black h-screen w-screen">
+    <div className=" h-screen w-screen">
       <Home />
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,21 +6,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { showLoader, hideLoader } from '../redux/loaderSlice';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import axios, * as others from 'axios';
+import Map from './Map';
 
 function Home() {
     // const { loader } = useSelector(state => state.loaderReducer);
-    axios.get('https://api.radar.io/v1/search/autocomplete?query=841+Broadway&near=43.6605883%2C-70.2601336', {
-        headers: {
-            Authorization: 'prj_live_pk_68b4a5fd8c30ffe24adfcc6e640c159d50213e4c'
-        }
-    })
-        .then(function (response) {
-            console.log(response);
-        })
     return (
         <div>
             {/* {!loader && <Loader />} */}
-
+            <Map />
         </div>
     )
 }
