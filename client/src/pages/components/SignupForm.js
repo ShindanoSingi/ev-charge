96b413f318aac1fe3aa1052d2fc184f02f8362af
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-06-09 00:35:33 -0400

    Still working on the form

--- client/src/pages/components/SignupForm.js
+++ client/src/pages/components/SignupForm.js
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setShowCard } from '../../redux/userSlice';
 import * as Yup from 'yup';
 
+const onSubmit = values => {
+    console.log(values);
+};
+
 const SignupForm = () => {
 
     const { showCard } = useSelector((state) => state.userReducer);
@@ -29,43 +33,52 @@ const SignupForm = () => {
                 }}
                 validationSchema={Yup.object({
                     username: Yup.string()
-                        .max(15, 'Must be 15 characters or less')
-                        .required('Required'),
+                        // .max(15, 'Must be 15 characters or less')
+                        // .username('Invalid username')
+                        .required('Required!'),
                     email: Yup.string()
                         .email('Invalid email address')
                         //.max(255, 'Must be 255 characters or less')
-                        .required('Required'),
+                        .required('Required!'),
                     password: Yup.string()
-                        .max(20, 'Must be 20 characters or less')
-                        .required('Required'),
+                        // .max(20, 'Must be 20 characters or less')
+                        .required('Required!'),
                 })}
             >
-                <Form className='flex flex-col gap-2 text-gray-400'>
-                    <label htmlFor="username">Username</label>
-                    <Field
-                        id="username"
-                        name="username"
-                        type="text"
-                        className='px-3 py-1'
-                    />
-                    <ErrorMessage name='name' className='text-red' />
-                    <label htmlFor="email">Email Address</label>
-                    <Field
-                        id="email"
-                        name="email"
-                        type="email"
-                        className='px-3 py-1'
-                    />
-                    <ErrorMessage name='email' className='text-red' />
-                    <label htmlFor="password">Password</label>
-                    <Field
-                        id="password"
-                        name="password"
-                        type="password"
-                        className='px-3 py-1'
-                    />
-                    <ErrorMessage name='password' className='text-red' />
-                    <button type="submit">Submit</button>
+                <Form className='flex flex-col gap-3 text-gray-400'>
+                    <div className='flex flex-col gap-0'>
+                        <label className='text-lg' htmlFor="username">Username</label>
+                        <Field
+                            id="username"
+                            name="username"
+                            type="text"
+                            className='px-3 py-2 rounded-t-xl'
+                        />
+                        <ErrorMessage name='username' />
+                    </div>
+                    <div className='flex flex-col gap-0 text-gray-400'>
+                        <label className='text-lg' htmlFor="email">Email Address</label>
+                        <Field
+                            id="email"
+                            name="email"
+                            type="email"
+                            className='px-3 py-2'
+                        />
+                        <ErrorMessage name='email' />
+                    </div>
+                    <div className='flex flex-col gap-0 text-gray-400'>
+                        <label className='text-lg' htmlFor="password">Password</label>
+                        <Field
+                            id="password"
+                            name="password"
+                            type="password"
+                            className='px-3 py-2 rounded-b-xl'
+                        />
+                        <ErrorMessage name='password' />
+                    </div>
+                    <div className='bg-gray-400 mt-2 p-1 text-center text-gray-800 text-xl rounded-xl'>
+                        <button onClick={onSubmit} type="submit">Submit</button>
+                    </div>
                 </Form>
             </Formik>
         </div>
