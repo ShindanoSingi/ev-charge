f6792949f5dfd494c36ebfbbe06411cd25deec8a
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-06-11 15:28:07 -0400

    added over effect on menu

--- client/src/pages/components/MenuButton.js
+++ client/src/pages/components/MenuButton.js
@@ -33,20 +33,28 @@ function MenuButton() {
                     <AiOutlineMenu className='text-gray-400 text-5xl font-thin' />
                 </Button>
                 <Menu
-                    className='mt-12'
+                    className='mt-[3rem] ml-[0.7rem]'
                     keepMounted
                     anchorEl={anchorEl}
                     onClose={handleClose}
                     open={Boolean(anchorEl)}
                 >
-                    <div>
+                    <div className=''>
+                        <p className='bg-gray-200 px-2 w-full hover:bg-black hover:text-white border-b-2 border-gray-400' onClick={handleClose}>My Account</p>
+                        <Link to='/signup'>
+                            <p className='bg-gray-200 hover:bg-black hover:text-white px-2 w-full border-b-2 border-gray-400' onClick={handleClose}>Sign Up</p>
+                        </Link>
+                        <p className='bg-gray-200 hover:bg-black hover:text-white px-2 w-full border-b-2 border-gray-400' onClick={handleClose}>Sign In</p>
+                        <p className='bg-gray-200 hover:bg-black hover:text-white px-2 w-full ' onClick={handleClose}>Sign Out</p>
+                    </div>
+                    {/* <div>
                         <li className='bg-gray-200 px-2 w-full' onClick={handleClose}>My Account</li><hr className='bg-gray-700' />
                         <Link to='/signup'>
-                            <li className='bg-gray-200 px-2 w-full' onClick={handleClose}>Sign Up</li><hr className='bg-gray-700' />
+                            <li className='bg-gray-200 px-2 w-full' onClick={handleClose}>Sign Up</li><hr className='bg-gray-800' />
                         </Link>
                         <li className='bg-gray-200 px-2 w-full' onClick={handleClose}>Sign In</li><hr className='bg-gray-700' />
                         <li className='bg-gray-200 px-2 w-full' onClick={handleClose}>Sign Out</li>
-                    </div>
+                    </div> */}
 
                 </Menu>
             </div>
